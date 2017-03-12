e22beff4b610e23a007e599e319ba0f37d2a584d
张思浩 <zhangsihao@zhangsiaodeiMac.lan>
2017-03-13 02:57:29 +0800

    Autoreplace .oh-my-zsh path

--- funcs/ohmyzsh.sh
+++ funcs/ohmyzsh.sh
@@ -1,6 +1,7 @@
 function ohmyzsh {
     if [ -d ~/.oh-my-zsh ]; then
         echo 'oh-my-zsh exists, apply settings'
-        cp -fv .zshrc ~/
+        local H=`echo $HOME|sed 's#/#\\\/#g'`
+        sed "s/^\(export ZSH=\).*$/\1$H\/.oh-my-zsh/" .zshrc > ~/.zshrc
     fi
 }
